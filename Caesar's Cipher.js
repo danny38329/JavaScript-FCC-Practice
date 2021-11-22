ae9d1c8897b717190c2986bcf9cc6d28ddc0de6a
Daniel Acosta <danielacosta10@aol.com>
2021-11-21 21:24:50 -0800

    Shifted indexes 13 numbers over

--- Caesar's Cipher.js
+++ Caesar's Cipher.js
@@ -2,7 +2,12 @@ function rot13(str) {
     let newArray = [];
     const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     for (let i = 0; i < str.length; i++) {
-      newArray.push(alphabet.indexOf(str[i])); 
+      if (alphabet.indexOf(str[i]) < 13) {
+        newArray.push(alphabet.indexOf(str[i])+13);
+      } else {
+        newArray.push(alphabet.indexOf(str[i])-13);
+      }
+      
     }
     return newArray;
   }
