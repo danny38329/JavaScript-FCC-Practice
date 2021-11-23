ed7f66f901ba2a10993cdb2534012ac81a2d4f77
Daniel Acosta <danielacosta10@aol.com>
2021-11-22 18:56:23 -0800

    Joined and rotated the string

--- Caesar's Cipher.js
+++ Caesar's Cipher.js
@@ -2,14 +2,17 @@ function rot13(str) {
     let newArray = [];
     const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     for (let i = 0; i < str.length; i++) {
-      if (alphabet.indexOf(str[i]) < 13) {
-        newArray.push(alphabet.indexOf(str[i])+13);
+      if (alphabet.indexOf(str[i]) === -1) {
+        newArray.push(" ")
+      }
+      else if (alphabet.indexOf(str[i]) < 13) {
+        newArray.push(alphabet[alphabet.indexOf(str[i])+13]);
       } else {
-        newArray.push(alphabet.indexOf(str[i])-13);
+        newArray.push(alphabet[alphabet.indexOf(str[i])-13]);
       }
       
     }
-    return newArray;
+    return newArray.join("");
   }
   
-  console.log(rot13("SERR PBQR PNZC"));
+  console.log(rot13("SERR PBQR9 PNZC"));
