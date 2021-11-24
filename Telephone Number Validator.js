30fb9c517c3eee61a030da75dbccacd969ed7041
Daniel Acosta <danielacosta10@aol.com>
2021-11-23 23:06:54 -0800

    Adding special characters array

--- Telephone Number Validator.js
+++ Telephone Number Validator.js
@@ -1,8 +1,12 @@
 function telephoneCheck(str) {
+    const specialCharsArr = ["<",">","/","?",":",";","{","}","[","]","!","@","#","$","%","^","&","*","(",")","_","-","+","=","~",];
+    const numberLine = [1,2,3,4,5,6,7,8,9];
     let numCount = 0;
     for (let i = 0; i < str.length; i++) {
       if (!isNaN(str[i])) {
         numCount += 1;
+      } else if (numberLine.indexOf(str[i])=== -1) {
+        return "No number " + numCount;
       }
     }
     return true;
