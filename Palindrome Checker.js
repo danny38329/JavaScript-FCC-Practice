a27369b5df1c9657b21259c527c47949a5e257e6
Daniel Acosta <danielacosta10@aol.com>
2021-11-25 15:35:22 -0800

    adding another project

--- Palindrome Checker.js
+++ Palindrome Checker.js
@@ -8,9 +8,12 @@ function palindrome(str) {
       } else {
           continue;
       }; 
-    
     }
-   return newString;
+    for (let x = str.length - 1; x >= 0; x--){
+        console.log(str[x]);
+    }
+
+   return str;
   }
   
   console.log(palindrome("ey999e"));
