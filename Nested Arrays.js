9fd333cb874f3ed4aed3b95db5a423b87eddff3f
Daniel Acosta <danielacosta10@aol.com>
2021-11-27 22:35:59 -0800

    Nested array practice

---
+++ Nested Arrays.js
@@ -0,0 +1,12 @@
+let nestArr = [[1,2,3,4,5],[6,7],[8]];
+
+
+for (let i = 0; i < nestArr.length; i++) {
+   
+    for (let x = 0; x < nestArr[i].length; x++) {
+        //console.log(nestArr[i].length);
+        //console.log(i);
+         console.log(nestArr[i][x]);
+        
+    };
+};
