f5c9939e9d4ecbe157d391f3ad0aa2e815b758eb
Daniel Acosta <danielacosta10@aol.com>
2021-11-27 23:13:42 -0800

    Sum all numbers between two numbers in an array

--- Sum All.js
+++ Sum All.js
@@ -1,9 +1,18 @@
 function sumAll(arr) {
-    let count = 0;
-    for (let i = arr[0]; i <= arr[arr.length - 1]; i++) {
+  let count = 0;
+ 
+  if (arr[0] < arr[1]) {
+    for (let i = arr[0]; i <= arr[1]; i++) {
       count += i;
+    }
+    return count;
+  } else {
+    for (let x = arr[0]; x >= arr[1]; x--) {
+      count += x;
     }
     return count;
   }
   
-  console.log(sumAll([1, 4]));
+}
+
+console.log(sumAll([6, 5]));
