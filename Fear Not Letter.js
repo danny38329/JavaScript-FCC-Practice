ee5b60576381e821745edf3ce678ee21ed650112
Daniel Acosta <danielacosta10@aol.com>
2021-12-03 20:13:55 -0800

    JS Practice

---
+++ Fear Not Letter.js
@@ -0,0 +1,10 @@
+function fearNotLetter(str) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let startAlph = alphabet.indexOf(str[0]);
+    for (let i = 0; i < str.length; i++) {
+  
+    }
+    return startAlph;
+  }
+  
+  console.log(fearNotLetter("abce"));
