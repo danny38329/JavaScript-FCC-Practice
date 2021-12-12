5a59ec8be83964579f4e3bf0ae99e39470725ae6
Daniel Acosta <danielacosta10@aol.com>
2021-12-11 23:38:34 -0800

    New algorithm with regional expressions used

--- Palindrome Checker.js
+++ Palindrome Checker.js
@@ -1,29 +1,14 @@
 function palindrome(str) {
-  let newString = "";
-  const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  if (str === "1 eye for of 1 eye.") {
-    return false;
+  
+  str = str.toLowerCase();  //Turn everything to lower case
+  // Remove all alphanumeric characters.
+  str = str.replace(/[^a-z0-9]/g, '');
+  var backward = str.split('');
+  backward = backward.reverse().join('').toString();
+  
+  if(backward === str){
+    return true;
   } else {
-
-  for (let i = 0; i < str.length; i++) {
-    if (alphabet.indexOf(str[i].toUpperCase()) !== -1) {
-      newString += str[i];
-    } else {
-        continue;
-    }; 
+    return false;
   }
-  console.log(newString);
-  const len = newString.length;
-
-  // loop through half of the string
-  for (let x = 0; x < len / 2; x++) {
-
-      // check if first and last string are same
-      if (newString[x].toLowerCase() !== newString[len - 1 - x].toLowerCase()) {
-          return false;
-      }
-  }
-  return true;
-}};
-
-console.log(palindrome("A man, a plan, a canal. Panama"))
+}
