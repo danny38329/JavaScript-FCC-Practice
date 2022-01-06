674f01f4ac159d3b9becda0dd85110c0c955a875
Daniel Acosta <danielacosta10@aol.com>
2022-01-05 23:09:54 -0800

    Codesignal practice

---
+++ Common Character Count.js
@@ -0,0 +1,10 @@
+function solution(s1, s2) {
+    let count = 0;
+    for(let i = 0; i < s1.length; i++){
+        if(s2.indexOf(s1[i]) != -1){
+            count++;
+            s2 = s2.replace(s1[i], '');
+        }
+    }
+    return count;
+}
